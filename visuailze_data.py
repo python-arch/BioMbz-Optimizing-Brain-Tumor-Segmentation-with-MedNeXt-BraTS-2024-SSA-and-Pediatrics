adc25490b9afde23b69c1374128a0cb17666303e
Abdelrahman El Sayed (python) <53436138+python-arch@users.noreply.github.com>
2024-07-30 11:20:32 +0300

    Update visuailze_data.py

--- visuailze_data.py
+++ visuailze_data.py
@@ -9,11 +9,11 @@ import matplotlib.gridspec as gridspec
 def load_data(data_dir, example_id, modalities=["t2f", "t1", "t1c", "t2w"]):
     images = []
     for modality in modalities:
-        nii_path = os.path.join(data_dir, example_id, f"{example_id}_{modality}.nii.gz") # get the path for each channel
+        nii_path = os.path.join(data_dir, example_id, f"{example_id}-{modality}.nii.gz") # get the path for each channel
         nii_img = nib.load(nii_path)
         print(nii_img.shape)
         images.append(nii_img.get_fdata()) # get the data
-    seg_path = os.path.join(data_dir, example_id, f"{example_id}_seg.nii.gz")
+    seg_path = os.path.join(data_dir, example_id, f"{example_id}-seg.nii.gz")
     seg_img = nib.load(seg_path)
     seg_data = seg_img.get_fdata()
     return images, seg_data
@@ -43,7 +43,7 @@ def plot_slices(images, seg_data, slice_index=None):
 
 # Example usage
 data_dir = "/home/abdelrahman.elsayed/Downloads/oriented_270_samples"
-example_id = "009"
+example_id = "BraTS-PED-00249-000"
 modalities = ["t2f", "t1n", "t1c", "t2w"]
 
 # Load data
